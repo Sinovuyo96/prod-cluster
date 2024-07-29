a16c97ae62013a14ef56b2b7c42a4967eb218e44
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-29 22:42:12 +0200

    Update prod.tf

--- modules/prod/prod.tf
+++ modules/prod/prod.tf
@@ -1,5 +1,5 @@
 module "prod-eks-cluster" {
-  source                      = "github.com/Sinovuyo96/cluster-001"
+  source                      = "github.com/Sinovuyo96/cluster-001/terraform?ref=main"
   terraform_state_bucket_name = "eks-application-state-bucket"
     
   # Networking configs
