336fdebaa17d1a9383a65d5fc85a6d204757d185
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-10-10 17:58:19 +0200

    Update prod.tf

--- modules/prod/prod.tf
+++ modules/prod/prod.tf
@@ -3,8 +3,8 @@ module "prod-eks-cluster" {
  # terraform_state_bucket_name = "eks-application-state-bucket"
     
   # Networking configs
-  vpc_id                      = "vpc-09d2f8c022beb848b"
-  subnet_ids                  = ["subnet-06818b2329a21cd3a", "subnet-050899c116d3c8050", "subnet-01a6100db3bf27588"]
+  vpc_id                      = "vpc-02d6568943b4a478e"
+  subnet_ids                  = ["subnet-09846cdc3bd541f1c", "subnet-0c895c55ee8422eff", "subnet-099ad55c9a9e69753"]
     
   # eks cluster settings      
   cluster_name                = "prod-cluster-002"
